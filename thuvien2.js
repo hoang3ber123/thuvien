2e32b7b9b9150dbd2792b7f1c53eea09fadeee55
hoang3ber123 <111274100+hoang3ber123@users.noreply.github.com>
2022-08-20 22:13:10 +0700

    Update thuvien2.js

--- thuvien2.js
+++ thuvien2.js
@@ -20,10 +20,44 @@ $(document).ready(function(){
         $(this).addClass("active")
         let id_tab=$(this).children("a").attr("href")
         $("div.introItem").hide();
-        $(id_tab).slideDown("slow")
+        $(id_tab).fadeIn("slow")
         $("html,body").animate({scrollTop:0});
     })
     $("div.close").click(function(){
         $("div.introItem").slideUp("slow")
+    })
+    $("ul.mainMenu >li >a").click(function(){
+        let id=$(this).attr("href")
+        $("html,body").animate({scrollTop:$(id).prop("offsetTop")+"px"},2000);
+    })
+    let img= $(".tacGia img")
+            img.fadeOut()
+            $(window).scroll(function(){
+                if($(this).scrollTop()>$("#tacPham").prop("offsetTop")-200){
+                    for(let i=0;i<img.length;i++)
+                        {
+                            let c=(i+1)*1000
+                            $(".tacGia img").eq(i).fadeIn(c)
+                        }
+                }
+                else{
+                    $(".tacGia img").hide()
+                }
+                
+    })
+    var count=0
+    $("div.buttonFooter").click(function(){
+        count++
+        if((count%2)!=0)
+        {
+            $("div.buttonFooter i").removeClass()
+            $("div.buttonFooter i").addClass("fa-solid fa-angles-up")
+        }
+        if((count%2)==0)
+        {
+            $("div.buttonFooter i").removeClass()
+            $("div.buttonFooter i").addClass("fa-solid fa-angles-down")
+        }
+        $("footer").slideToggle(2000)
     })
 })
